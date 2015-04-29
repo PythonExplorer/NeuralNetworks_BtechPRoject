d49173d659eabe21644035c5c1cd58ceae47a475
infinitum-rakesh <rakeshnitcalicut@gmail.com>
2015-04-29 15:14:06 +0530

    Random Initialization of Weights

--- FinalCode.py
+++ FinalCode.py
@@ -1,8 +1,13 @@
+#References
+#No of Hidden Layers and Nodes : http://goo.gl/OdBRY2
+#No of Hidden Layers and Nodes : http://goo.gl/Nb4GZp
+
+
+#Header Files and Imports
+import random
+
 #Reading the Data from txt files
 #ref : https://docs.python.org/2/tutorial/inputoutput.html#reading-and-writing-files
-
-
-
 def prepareInput(act_file,inact_file):
     active_TD = open(act_file,'r')
     inactive_TD = open(inact_file,'r')
@@ -33,7 +38,25 @@ def prepareInput(act_file,inact_file):
             break
     return (DataMatrix, OutputMatrix)
 
+#Intialize the weights to some random value before starting the training of the model
+def initializeRandomWeights(row,col):
+    weights= []
+    for i in range(0,row):
+        r = []
+        for j in range(0, col):
+            r.append(random.uniform(10, 11))
+        weights.append(r)
+    return weights    
 
+
+#Input Raw Data and get Preprocessed Data
 TD, TR = prepareInput('Datasets/1332/1332- active.txt','Datasets/1332/1332- inactive.txt')
-print(len(TD))
-print(len(TR)) 
+
+#Intialize the weights for both layers
+rows = len(TD[0])
+cols = len(TD)/2
+WL1 = initializeRandomWeights(rows,cols)
+WL2 = initializeRandomWeights(rows,cols)
+
+
+
